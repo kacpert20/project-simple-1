d5223cf046bb0ff10bb31125e1c0c5f55a4b3dc1
kacpert20 <668918250kt@gmail.com>
2021-02-22 08:41:29 +0100

    faster ball and winable AI

--- project-simple-04.py
+++ project-simple-04.py
@@ -1,7 +1,7 @@
 # to jest komentarz
 import pgzrun
 from random import randint, choice
-
+bounces = 0
 class Paletka:
 
     def __init__(self, paletka, pozycja):
@@ -13,25 +13,33 @@ class Paletka:
 
     def move(self, direction):
         if direction == "left":
-            self.paletka.x -= 5
+            self.paletka.x -= 7
         elif direction == "right":
-            self.paletka.x += 5
+            self.paletka.x += 7
 
     def bounce(self):
+        global bounces
         # taki prosty zabieg nie wystarczy
         if self.paletka.distance_to(ball) > 80:
             return False
-
         if abs(self.paletka.y - ball.y) > 20:
             return False
-
+        bounces += 1
         return True
 
 def aktualizuj_pozycje_paletek():
-    if keyboard.a:
-        paletka_a.move("left")
-    if keyboard.s:
-        paletka_a.move("right")
+    if paletka_a.AI == False:
+        if keyboard.a:
+            paletka_a.move("left")
+        if keyboard.s:
+            paletka_a.move("right")
+
+    else:
+        if (ball.x > paletka_a.paletka.x):
+            paletka_a.paletka.x += 5
+        if (ball.x < paletka_a.paletka.x):
+            paletka_a.paletka.x -= 5
+
     if keyboard.k:
         paletka_b.move("left")
     if keyboard.l:
@@ -73,8 +81,8 @@ def sprawdz_czy_odbijemy():
 WIDTH = 1280
 HEIGHT = 853
 
-paletka_a = Paletka(Actor("palette.png"), (100,20))
-paletka_b = Paletka(Actor("palette.png"), (100,833))
+paletka_a = Paletka(Actor("palette.png"), ((WIDTH/2),20))
+paletka_b = Paletka(Actor("palette.png"), ((WIDTH/2),833))
 
 ball = Actor("ball.png")
 ball.x = randint(40, WIDTH - 40)
@@ -86,26 +94,41 @@ ball.stop = False
 ball.winner = None
 ball.direction_x = choice(("left", "right"))
 ball.direction_y = choice(("up", "down"))
-ball.speed = 2
+ball.speed = 4
+paletka_a.AI = False
 
 def draw():
 
     screen.blit("desert.jpg",(0,0))
     if not ball.start:
         screen.draw.text(
-            "Naciśnij SPACJĘ aby rozpocząć.", (40, 150), fontsize=40, color=(0, 0, 0)
+            "Naciśnij SPACJĘ aby rozpocząć. Wciśnij P, aby grać z komputerem.", (40, 150), fontsize=40, color=(0, 0, 0)
         )
     paletka_a.drawing()
     paletka_b.drawing()
     ball.draw()
 
 def update():
-    if not ball.start and keyboard.space:
-        ball.game_run = True
-        ball.start = True
+    global bounces
+    if keyboard.p:
+        paletka_a.AI = True
+    if ball.game_run == False:
+        if not ball.start and keyboard.space:
+            ball.game_run = True
+            ball.start = True
     if ball.game_run:
         update_ball_position()
         aktualizuj_pozycje_paletek()
         sprawdz_czy_odbijemy()
+    if bounces == 7:
+        ball.speed += 1
+        bounces += 1
+    elif bounces == 15:
+        ball.speed += 1
+        bounces += 1
+    elif bounces == 20:
+        ball.speed += 1
+        bounces += 1
+
 
 pgzrun.go()
